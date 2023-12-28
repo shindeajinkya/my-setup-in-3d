6b2e42e5512eb09cab3281b80c67c1dfb43dfd9a
shindeajinkya <ajitshinde635@gmail.com>
2023-12-28 15:24:31 +0530

    fix: unused variable

--- src/main.ts
+++ src/main.ts
@@ -123,9 +123,9 @@ gltfLoader.load("/my-room-in-3d.glb", (gltf) => {
 });
 
 // Geometry
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
+// const cube = new THREE.Mesh(geometry, material);
 // scene.add(cube);
 
 camera.position.x = 3;
